2937304a49c98dcee32a2245ce57b1c803d3cf18
Aaditya Aryal <aryalaadi123@gmail.com>
2024-03-09 18:54:10 +0545

    src: zinit source

---
+++ xv6std/xv6std.zig
@@ -0,0 +1,14 @@
+pub extern fn fork() c_int;
+pub extern fn exec([*c]const u8, [*c][*c]u8) c_int;
+pub extern fn exit(c_int) noreturn;
+pub extern fn wait([*c]c_int) c_int;
+pub extern fn open([*c]const u8, c_int) c_int;
+pub extern fn mknod([*c]const u8, c_short, c_short) c_int;
+pub extern fn dup(c_int) c_int;
+pub extern fn printf([*c]const u8, ...) void;
+
+pub const CONSOLE = @as(c_int, 1);
+pub const O_RDONLY = @as(c_int, 0x000);
+pub const O_WRONLY = @as(c_int, 0x001);
+pub const O_RDWR = @as(c_int, 0x002);
+pub const O_CREATE = @as(c_int, 0x200);
---
+++ zinit.zig
@@ -0,0 +1,39 @@
+// zinit: xv6-riscv init system in zig
+const std = @import("xv6std/xv6std.zig");
+
+pub export fn main() u8 {
+    var pid: c_int = undefined;
+    var wpid: c_int = undefined;
+    if (std.open("console", std.O_RDWR) != 0) {
+        _ = std.mknod("console", std.CONSOLE, 0);
+        _ = std.open("console", std.O_RDWR);
+    }
+
+    _ = std.dup(0); // stdout
+    _ = std.dup(0); // stderr
+
+    const argv = &[_:null]?[*:0]const u8{"sh"};
+    while (true) {
+        std.printf("zinit: starting sh\n");
+        pid = std.fork();
+        if (pid < 0) {
+            std.printf("zinit: fork failed\n");
+            _ = std.exit(1);
+        }
+        if (pid == 0) {
+            _ = std.exec("sh", @constCast(@ptrCast(argv)));
+            std.printf("zinit: exec sh failed\n");
+            _ = std.exit(1);
+        }
+        while (true) {
+            wpid = std.wait(@as([*c]c_int, 0));
+            if (wpid == pid) {
+                break;
+            } else if (wpid < 0) {
+                std.printf("zinit: wait returned an error\n");
+                _ = std.exit(1);
+            } else {}
+        }
+    }
+    return 0;
+}
